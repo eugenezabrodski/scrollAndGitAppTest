42f282a93438201b640c587ebaa4cdea8589d263
eugenezabrodski <115879896+eugenezabrodski@users.noreply.github.com>
2022-10-27 21:33:35 +0300

    dev2-2

--- scrollView/ViewController.swift
+++ scrollView/ViewController.swift
@@ -12,6 +12,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         print("im second but not last")
+        print("Something at second")
     }
 
 
